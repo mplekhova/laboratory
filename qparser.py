cddaee8f0a39b00bc9026725a0d8ee22b173d69f
Marina Plekhova <50870211+mplekhova@users.noreply.github.com>
2020-03-29 12:51:30 +0300

    edited

--- qparser.py
+++ qparser.py
@@ -8,19 +8,23 @@ import requests
 def get_source_code(agent, url):
   agent.get(url)
   source_code = fromstring(agent.page_source)
-  # print(source_code)
   return source_code
+
+
 # this func goes through 40 pages and gets about 10 links on articles from each page
-def get_articles_urls(agent, source_code, links, i):
+def get_articles_urls(agent, source_code, links):
   temp_list_of_links = (source_code.xpath("//h2[@class='entry-title']/a/@href"))
+  
   # links on articles to be parsed 
   for elem in temp_list_of_links:
     links.append(elem)
-  if i != 39:
+
+  if source_code.xpath("//*[@id='content']/div[12]/div/a[@class='nextpostslink']/@href"):
     # link on next page with other 10 articles
     next_page = source_code.xpath("//*[@id='content']/div[12]/div/a[@class='nextpostslink']/@href")
     return (links, next_page[0])
   return links
+
 
 if __name__ == '__main__':
     driver = webdriver.Firefox(executable_path='geckodriver',  
@@ -29,22 +33,20 @@ if __name__ == '__main__':
     source_code =  get_source_code(driver, url)
     links = []
 
-    for i in range(40):
-      if i != 39:
-        links, next_page = get_articles_urls(driver, source_code, links, i)
-        source_code = get_source_code(driver, next_page)
-      else:
-        links = get_articles_urls(driver, source_code, links, i)
+    while source_code.xpath("//*[@id='content']/div[12]/div/a[@class='nextpostslink']/@href"):
+      links, next_page = get_articles_urls(driver, source_code, links)
+      source_code = get_source_code(driver, next_page)
+    
+    links = get_articles_urls(driver, source_code, links)
     driver.quit()
+
   # all links are put into json file
     article_link_data = {}
-    for i in range(394):
+    for i in range(len(links)):
       article_link_data[str(i)] = links[i]
-    print(len(article_link_data))
     with open('articles.json', 'w', encoding='utf-8') as f:
       json.dump(article_link_data, f, indent=4)
 
-  # now we have links on 394 article and we'll parse them
     
 
     
